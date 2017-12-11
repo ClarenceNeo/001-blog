c107f0187720927fa486ed6cb7e1e7ff68b2d12e
ClarenceNeo <niuzhiyuan94@gmail.com>
2017-12-11 20:21:25 +0800

    add css

--- view/home.php
+++ view/home.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
   <?php tpl('component/css'); ?>
   <link rel="stylesheet" href="css/home.css">
   <title>blog</title>
@@ -17,6 +18,10 @@
       <div class="content">
         <h2><a class="title" href="#">标题标题标题</a></h2>
         <p>Article contentArticle contentArticle contentArticle contentArticle contentArticle contentArticle contentArticle contentArticle contentArticle contentArticle content</p>
+        <div class="post_footer">
+          <span>2017.12.11</span>
+          <span class="tags"><i class="fa fa-tags"></i><a href="#">tag1</a> <a href="#">tag2</a></span>
+        </div>
       </div>
     </div>
   </div>
