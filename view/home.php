7e7d63b0dd45a370dbcda600698f833d71be5042
ClarenceNeo <niuzhiyuan94@gmail.com>
2017-12-11 17:05:59 +0800

    add css

--- view/home.php
+++ view/home.php
@@ -4,9 +4,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <?php tpl('component/css'); ?>
+  <link rel="stylesheet" href="css/home.css">
   <title>blog</title>
 </head>
 <body>
-  <h1>首页</h1>
+  <div class="container">
+    <div class="wrapper">
+      <div class="header">
+        <h1>BLOG</h1>
+      </div>
+      <div class="content">
+        <h2><a class="title" href="#">标题标题标题</a></h2>
+        <p>Article contentArticle contentArticle contentArticle contentArticle contentArticle contentArticle contentArticle contentArticle contentArticle contentArticle content</p>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+  </div>
 </body>
 </html>
